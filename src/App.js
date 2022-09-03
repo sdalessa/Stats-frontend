09ff6d3dd8fee2374f3da72b678c9a402b8d8e67
Stefano D'Alessandro <stefanodalessandro@Stefanos-MacBook-Air-2.local>
2022-09-03 14:19:58 +0200

    frontend6

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 //Need to check whether this installation in the main folder, instead of the frontend folder works for the ui icons
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
 import { useContext } from "react";
 
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,34 @@
+import axios from "axios";
+import { useRef } from "react";
 import "./register.css";
+import useHistory from "react-router-dom";
 
 export default function Register() {
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const passwordAgain = useRef();
+  const history = useHistory();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (passwordAgain.current.value !== password.current.value) {
+      passwordAgain.current.setCustomValidity("Passwords do not match!");
+    } else {
+      const user = {
+        username: username.current.value,
+        email: email.current.value,
+        password: password.current.value
+      };
+      try{
+      const res = axios.post("/auth/register", user);
+      history.push("/login")
+    }catch(err){
+      console.log(err)
+    }
+    }
+  };
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,16 +39,42 @@ export default function Register() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Username" className="loginInput" />
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <input placeholder="Repeat Password" className="loginInput" />
-            <button className="loginButton">Sign Up</button>
+          <form className="loginBox" onSubmit={handleClick}>
+            <input
+              placeholder="Username"
+              required
+              ref={username}
+              className="loginInput"
+            />
+            <input
+              placeholder="Email"
+              required
+              ref={email}
+              className="loginInput"
+              type="email"
+            />
+            <input
+              placeholder="Password"
+              required
+              ref={password}
+              className="loginInput"
+              type="password"
+              minLength="6"
+            />
+            <input
+              placeholder="Repeat Password"
+              required
+              ref={passwordAgain}
+              className="loginInput"
+              type="password"
+            />
+            <button className="loginButton" type="submit">
+              Sign Up
+            </button>
             <button className="registerButton" id="">
               Log into your Account
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
